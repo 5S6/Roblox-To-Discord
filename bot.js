890f2d56719eb285cc6dd95dc38b95ac877cabd0
AlekDevs <amacwilliams06@gmail.com>
2021-10-07 12:14:42 -0400

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,111 @@
+var Discord = require('discord.js');
+var rbxbot = require('noblox.js');
+var bot = new Discord.Client();
+const fs = require('fs')
+var config = require('./config.json')
+bot.commands = new Discord.Collection()
+const fetch = require("node-fetch");
+
+const commandfiles = fs.readdirSync('./Commands/').filter(file => file.endsWith('.js'));
+
+for (const file of commandfiles){
+    const command = require(`./Commands/${file}`)
+    bot.commands.set(command.name,command);
+}
+
+bot.on ('message', (message) => {
+
+var msg = message.content.toLocaleLowerCase()
+var prefix = "!"
+var args = message.content.split(/ +/)
+
+if(message.author.bot) return;
+if(message.channel.type === 'dm') return ;
+
+if (msg.startsWith(prefix + 'userinfo')){
+    bot.commands.get('UserInfo').execute(message,msg);
+}
+
+if (msg.startsWith(prefix + 'rank')) {
+    if(message.member.roles.cache.has('860249116754837525')){
+    bot.commands.get('Rank').execute(message,msg,args,config);}
+
+    else message.reply("You Don't Have Perms To Use This")
+}
+
+if(msg.startsWith(prefix + 'payrobux'))return bot.commands.get('PayRobux').execute(message,msg,args);
+
+
+if(msg.startsWith(prefix + 'shout'))return bot.commands.get('GroupShout').execute(message,msg,args);
+
+
+});
+
+
+
+
+
+
+
+bot.on('ready', () => {
+
+    console.log("The Bot Is Online!")
+
+});
+
+
+
+
+
+
+
+bot.on ('ready', async() => { 
+    bot.user.setActivity("Vertex", {type : "WATCHING"})
+    await rbxbot.setCookie(config.Cookie)
+    .then(async(success) => { // Required if the group's shout is private
+        console.log('Logged in.');
+ 
+        let onShout = rbxbot.onShout(config.GroupID);
+ 
+        onShout.on('data', async function(post) {
+ 
+    function GetAvatarURL(user)
+    {
+        return new Promise((resolve, reject) => 
+        {
+            fetch(`https://thumbnails.roblox.com/v1/users/avatar-headshot?format=Png&isCircular=false&size=352x352&userIds=${user}`)
+            .then(res => res.json())
+            .then(json => {
+              resolve (json.data[0].imageUrl)
+            })
+            .catch(reject);
+        });
+    }
+    
+    let avatarurl = await (GetAvatarURL(post.poster.userId))
+    const shoutchannel = await bot.guilds.cache.get('860231250996101120').channels.cache.get('865761129296822293')
+    const embed = new Discord.MessageEmbed()
+    .setTitle('Group Shout')
+    .setURL('https://www.roblox.com/groups/9791038/All-Of-Aleks-Alts#!/about')
+    .setDescription(post.body)
+    .setAuthor(post.poster.username, avatarurl)
+    shoutchannel.send(embed)
+    console.log(`${post.poster.username} posted ${post.body}`)
+});
+ 
+onShout.on('error', function (err) {
+    //console.log(err.message);
+});
+ 
+ 
+ 
+        })  
+ 
+    .catch((err) => console.error(err.message)); 
+    
+ 
+});
+
+
+
+bot.login(config.Token)
